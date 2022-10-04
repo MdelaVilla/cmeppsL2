6cc76817ff17929f56b8c5d0bf6c21b69775bf08
usuario <usuario@pc21-109>
2022-10-04 16:59:49 +0200

    Saludo al usuario

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -4,6 +4,9 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola CMEPPS!!!");
+		
+		String name="Manuel";
+		System.out.println("Bienvenido, "+name);
 
 	}
 
